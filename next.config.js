79f9f20e5575275b38dae854cd31f9d5c13c7f16
AnhTuanNguyen1706 <atuannguyen1706@gmail.com>
2023-07-17 23:50:02 +0700

    Revert "first commit"
    This reverts commit ca44400e2c8ae7c6b30bcfdea76ddbe4f160713b.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
+const nextConfig = {
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')]
+  // }
 };
+
+module.exports = nextConfig;
